2cbb1764a7174a7aaa09f484c157ea8d49c45497
tzr897 <ziruitang897@gmail.com>
2022-07-26 23:47:02 -0700

    Http_conn 3

--- http_conn.cpp
+++ http_conn.cpp
@@ -411,4 +411,96 @@ bool Http_conn::add_content_length(int content_len) {
     return add_response("Content-Length: %d\r\n", content_len);
 }
 
+bool Http_conn::add_linger() {
+    return add_response("Connection: %s\r\n", (m_linger == true) ? "keep-alive" : "close");
+}
+
+bool Http_conn::add_blank_line() {
+    return add_response("%s", "\r\n");
+}
+
+bool Http_conn::add_content(const char *content) {
+    return add_response("%s", content);
+}
+
+bool Http_conn::add_content_type() {
+    return add_response("Content-Type:%s\r\n", "text/html");
+}
+
+// depending on result of processing HTTP request, decide the content return to client
+bool Http_conn::process_write(HTTP_CODE ret) {
+    switch (ret) {
+        case INTERNAL_ERROR : {
+            add_status_line(500, error_500_title);
+            add_headers(strlen(error_500_form) );
+            if (!add_content(error_500_form)) {
+                return false;
+            }
+            break;
+        }
+
+        case BAD_REQUEST : {
+            add_status_line(400, error_400_title);
+            add_headers(strlen(error_400_form));
+            if (!add_content(error_400_form)) {
+                return false;
+            }
+            break;
+        }
+
+        case NO_RESOURCE : {
+            add_status_line(404, error_404_title);
+            add_headers(strlen(error_404_form));
+            if (!add_content(error_404_form)) {
+                return false;
+            }
+            break;
+        }
+
+        case FORBIDDEN_REQUEST : {
+            add_status_line(403, error_403_title);
+            add_headers(strlen(error_403_form));
+            if (!add_content(error_403_form)) {
+                return false;
+            }
+            break;
+        }
+
+        case FILE_REQUEST : {
+            add_status_line(200, ok_200_title);
+            add_headers(m_file_stat.st_size);
+            m_iv[0].iov_base = m_write_buf;
+            m_iv[0].iov_len = m_write_idx;
+            m_iv[1].iov_base = m_file_address;
+            m_iv[1].iov_len = m_file_stat.st_size;
+            m_iv_count = 2;
+            return true;
+        }
+
+        default: {
+            return false;
+        }
+    }
+    m_iv[0].iov_base = m_write_buf;
+    m_iv[0].iov_len = m_write_idx;
+    m_iv_count = 1;
+    return true;
+}
+
+// called by working thread in the thread pool
+void Http_conn::process() {
+    HTTP_CODE read_ret = process_read();
+    if (read_ret == NO_REQUEST) {
+        modfd(m_epollfd, m_sockfd, EPOLLIN);
+        return;
+    }
+
+    // generate the response
+    bool write_ret = process_write(read_ret);
+    if (!write_ret) {
+        close_conn();
+    }
+    modfd(m_epollfd, m_sockfd, EPOLLOUT);
+}
+
 
